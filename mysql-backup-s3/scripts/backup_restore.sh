0602c2e9b9ce9a7523db99f13bb02f4dd5e1433b
Deniz  Dagtekin <deniz.dagtekin@aoe.com>
2017-08-25 17:03:44 +0200

    Added missing aws cli parameter

--- mysql-backup-s3/scripts/backup_restore.sh
+++ mysql-backup-s3/scripts/backup_restore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-aws  --region "${AWS_DEFAULT_REGION}" cp "s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz" /tmp/dump.sql.gz && gunzip -c /tmp/dump.sql.gz | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
+aws  --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz" /tmp/dump.sql.gz && gunzip -c /tmp/dump.sql.gz | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
