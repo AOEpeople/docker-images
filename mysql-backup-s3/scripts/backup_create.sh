883907dc1050245192ae78d79a5ae39721c4c5a5
Fabrizio Branca <mail@fabrizio-branca.de>
2017-08-28 13:13:20 +0200

    Added debug output

--- mysql-backup-s3/scripts/backup_create.sh
+++ mysql-backup-s3/scripts/backup_create.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
-  mysqldump -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} | gzip > /tmp/dump.sql.gz && aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
+echo "Creating dump from ${DBUSER}:*****@${DBHOST}/${DBNAME}"
+mysqldump -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} | gzip > /tmp/dump.sql.gz
+
+echo "Uploading file to s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
+aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
--- mysql-backup-s3/scripts/backup_restore.sh
+++ mysql-backup-s3/scripts/backup_restore.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
-aws  --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz" /tmp/dump.sql.gz && gunzip -c /tmp/dump.sql.gz | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
+echo "Downloading file s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
+aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz" /tmp/dump.sql.gz
+
+echo "Importing dump to ${DBUSER}:*****@${DBHOST}/${DBNAME}"
+gunzip -c /tmp/dump.sql.gz | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
