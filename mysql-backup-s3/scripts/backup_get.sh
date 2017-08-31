16c4c5d35dcf65631a8a8437a30d7abb8e592be1
Deniz  Dagtekin <deniz.dagtekin@aoe.com>
2017-08-31 14:15:02 +0200

    Rename S3_PREFIX parameter

--- mysql-backup-s3/scripts/backup_get.sh
+++ mysql-backup-s3/scripts/backup_get.sh
@@ -2,6 +2,3 @@
 
 echo "Downloading file s3://${S3_BUCKET}${S3_KEY}"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_KEY}" /tmp/dump.sql.gz
-
-echo "Importing dump to ${DBUSER}:*****@${DBHOST}/${DBNAME}"
-gunzip -c /tmp/dump.sql.gz | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
--- mysql-backup-s3/scripts/backup_sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-echo "Downloading file s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
-aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz" /tmp/dump.sql.gz
