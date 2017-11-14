a5e12ccdaaf20352303337fc14f7bef87759f76d
Fabrizio Branca <fabrizio.branca@aoe.com>
2017-11-14 09:55:07 +0100

    Filter DEFINER comments

--- mysql-backup-s3/scripts/backup_create.sh
+++ mysql-backup-s3/scripts/backup_create.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
 echo "Creating dump from ${DBUSER}:*****@${DBHOST}/${DBNAME}"
-mysqldump -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | gzip > /tmp/dump.sql.gz
+mysqldump \
+   --single-transaction --quick \
+   -u${DBUSER} -p${DBPASSWORD} - h${DBHOST} ${DBNAME} \
+   | LANG=C LC_CTYPE=C LC_ALL=C sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' \
+   | gzip > /tmp/dump.sql.gz
 
 echo "Uploading file to s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_KEY}"
