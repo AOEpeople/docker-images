129aa303a8c2b401f1726017162d604894153c81
Fabrizio Branca <fabrizio.branca@aoe.com>
2017-11-14 09:41:17 +0100

    Filter DEFINER comments

--- mysql-backup-s3/scripts/backup_create.sh
+++ mysql-backup-s3/scripts/backup_create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "Creating dump from ${DBUSER}:*****@${DBHOST}/${DBNAME}"
-mysqldump -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} | gzip > /tmp/dump.sql.gz
+mysqldump -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | gzip > /tmp/dump.sql.gz
 
 echo "Uploading file to s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_KEY}"
--- mysql-backup-s3/scripts/backup_restore.sh
+++ mysql-backup-s3/scripts/backup_restore.sh
@@ -4,4 +4,4 @@ echo "Downloading file s3://${S3_BUCKET}${S3_KEY}"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_KEY}" /tmp/dump.sql.gz
 
 echo "Importing dump to ${DBUSER}:*****@${DBHOST}/${DBNAME}"
-gunzip -c /tmp/dump.sql.gz | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
+gunzip -c /tmp/dump.sql.gz | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
