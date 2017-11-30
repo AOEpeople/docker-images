517f8c221edb4f92a6a1c247fdd5ac5573cefd79
Fabrizio <gitkraken@fabrizio-branca.de>
2017-11-30 11:15:17 +0100

    Whitespace fix :)

--- mysql-backup-s3/scripts/backup_create.sh
+++ mysql-backup-s3/scripts/backup_create.sh
@@ -3,9 +3,22 @@
 echo "Creating dump from ${DBUSER}:*****@${DBHOST}/${DBNAME}"
 mysqldump \
    --single-transaction --quick \
-   -u${DBUSER} -p${DBPASSWORD} - h${DBHOST} ${DBNAME} \
+   -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} \
    | LANG=C LC_CTYPE=C LC_ALL=C sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' \
    | gzip > /tmp/dump.sql.gz
 
+FILESIZE=$(stat -c%s "/tmp/dump.sql.gz")
+
+if [ "$FILESIZE" -lt "1000" ] ; then
+  echo "Database dump too small ($FILESIZE)"
+  exit 1
+fi
+
 echo "Uploading file to s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_KEY}"
+
+
+export DBUSER=$PIM_DATABASE_USER
+export DBNAME=$PIM_DATABASE_NAME
+export DBPASSWORD=$PIM_DATABASE_PASSWORD
+export DBHOST=$PIM_DATABASE_HOST
