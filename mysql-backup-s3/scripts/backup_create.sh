4aff6c4421720c03dbd7c2e875ae1934291e7326
Fabrizio <gitkraken@fabrizio-branca.de>
2017-11-30 11:43:14 +0100

    Fixed comment and removed test code

--- mysql-backup-s3/scripts/backup_create.sh
+++ mysql-backup-s3/scripts/backup_create.sh
@@ -14,11 +14,5 @@ if [ "$FILESIZE" -lt "1000" ] ; then
   exit 1
 fi
 
-echo "Uploading file to s3://${S3_BUCKET}${S3_PREFIX}dump.sql.gz"
+echo "Uploading file to s3://${S3_BUCKET}${S3_KEY}"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_KEY}"
-
-
-export DBUSER=$PIM_DATABASE_USER
-export DBNAME=$PIM_DATABASE_NAME
-export DBPASSWORD=$PIM_DATABASE_PASSWORD
-export DBHOST=$PIM_DATABASE_HOST
