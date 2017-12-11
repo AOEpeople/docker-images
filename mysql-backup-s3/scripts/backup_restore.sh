7aff30d6a63131604e995e51f3a7fdc5236fa54d
Fabrizio <gitkraken@fabrizio-branca.de>
2017-12-11 12:31:54 +0100

    Added filesize check while restoring

--- mysql-backup-s3/scripts/backup_restore.sh
+++ mysql-backup-s3/scripts/backup_restore.sh
@@ -3,6 +3,13 @@
 echo "Downloading file s3://${S3_BUCKET}${S3_KEY}"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_KEY}" /tmp/dump.sql.gz
 
+FILESIZE=$(stat -c%s "/tmp/dump.sql.gz")
+
+if [ "$FILESIZE" -lt "1000" ] ; then
+  echo "Database dump too small ($FILESIZE)"
+  exit 1
+fi
+
 echo "Importing dump to ${DBUSER}:*****@${DBHOST}/${DBNAME}"
 gunzip -c /tmp/dump.sql.gz \
   | LANG=C LC_CTYPE=C LC_ALL=C sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' \
