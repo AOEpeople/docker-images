f3fb516cc4ba6fceb2c09c1d3e0b8a372f46ab83
Fabrizio Branca <fabrizio.branca@aoe.com>
2019-05-28 17:10:12 +0200

    Added debug output and disabling foreign key checks

--- mysql-backup-s3/scripts/backup_create.sh
+++ mysql-backup-s3/scripts/backup_create.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
+function echoerr { echo "============================================" 1>&2; echo "ERROR: $*" 1>&2;  echo "============================================" 1>&2; }
+function error_exit { echoerr "$1"; exit 1; }
+
 echo "Creating dump from ${DBUSER}:*****@${DBHOST}/${DBNAME}"
 mysqldump \
    --single-transaction --quick \
    -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} \
    | LANG=C LC_CTYPE=C LC_ALL=C sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' \
-   | gzip > /tmp/dump.sql.gz
+   | gzip > /tmp/dump.sql.gz || error_exit "Failed creating database dump"
 
 FILESIZE=$(stat -c%s "/tmp/dump.sql.gz")
 
-if [ "$FILESIZE" -lt "1000" ] ; then
-  echo "Database dump too small ($FILESIZE)"
-  exit 1
-fi
+if [ "$FILESIZE" -lt "1000" ] ; then error_exit "Database dump too small ($FILESIZE)"; fi
 
 echo "Uploading file to s3://${S3_BUCKET}${S3_KEY}"
-aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_KEY}"
+aws --region "${AWS_DEFAULT_REGION}" s3 cp /tmp/dump.sql.gz "s3://${S3_BUCKET}${S3_KEY}" || error_exit "Failed uploading dump to S3"
--- mysql-backup-s3/scripts/backup_restore.sh
+++ mysql-backup-s3/scripts/backup_restore.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -e
 
+function echoerr { echo "============================================" 1>&2; echo "ERROR: $*" 1>&2;  echo "============================================" 1>&2; }
+function error_exit { echoerr "$1"; exit 1; }
+
+echo "Importing s3://${S3_BUCKET}${S3_KEY} to ${DBUSER}:*****@${DBHOST}/${DBNAME}"
+
+
 echo "Downloading file s3://${S3_BUCKET}${S3_KEY}"
-aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_KEY}" /tmp/dump.sql.gz
+aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_KEY}" /tmp/dump.sql.gz || error_exit "Failed downloading dump from S3 bucket"
 
 FILESIZE=$(stat -c%s "/tmp/dump.sql.gz")
 
-if [ "$FILESIZE" -lt "1000" ] ; then
-  echo "Database dump too small ($FILESIZE)"
-  exit 1
-fi
+if [ "$FILESIZE" -lt "1000" ] ; then error_exit "Database dump too small ($FILESIZE)"; fi
 
-echo "Importing dump to ${DBUSER}:*****@${DBHOST}/${DBNAME}"
+echo "Disabling foreign key checks"
+mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} -e "SET FOREIGN_KEY_CHECKS=0" || error_exit "Failed disabling foreign keys"
+
+echo "Importing dump"
 gunzip -c /tmp/dump.sql.gz \
   | LANG=C LC_CTYPE=C LC_ALL=C sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' \
-  | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME}
+  | mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} || error_exit "Failed importing database dump"
+
+echo "Enabling foreign key checks"
+mysql -u${DBUSER} -p${DBPASSWORD} -h${DBHOST} ${DBNAME} -e "SET FOREIGN_KEY_CHECKS=1" || error_exit "Failed enabling foreign keys"
