12771918813ae584cfb0990a46e376f16a204bbd
Julian Kleinhans <julian.kleinhans@aoe.com>
2019-05-29 14:42:16 +0200

    Update aws_s3_objects prom metric script

--- prometheus-metrics/README.md
+++ prometheus-metrics/README.md
@@ -23,10 +23,6 @@ vuku k run check-acm-certificates --rm -it \
     --env PROM_PUSHGATEWAY_URL="http://prometheus-pushgateway.k28s-infrastructure" \
     --env AWS_DEFAULT_REGION="eu-central-1" \
     --image=aoepeople/prometheus-metrics:latest -- /bin/bash -c "/usr/local/bin/check_acm_certificates.sh"
-vuku k run check-acm-certificates --rm -it \
-    --env PROM_PUSHGATEWAY_URL="http://prometheus-pushgateway.k28s-infrastructure" \
-    --env AWS_DEFAULT_REGION="eu-central-1" \
-    --image=aoepeople/prometheus-metrics:latest -- /bin/bash 
 ```
 
 **Example command within localhost:**
@@ -70,27 +66,38 @@ docker run --rm -i -v $(PWD):/app -v $HOME/.aws:/root/.aws -w /app -e AWS_PROFIL
 
 Job: 
 ```
-s3_objects
+aws_s3_objects
 ```
 
 Metrics: 
 ```
-s3_key_age{bucket="<AWS_BUCKET_NAME>", key="<KEY>"} <AGE_IN_SECONDS>
-s3_key_size{bucket="<AWS_BUCKET_NAME>", key="<KEY>"} <OBJECT_SIZE_IN_BYTES>
+aws_s3_object_key_age{bucket="<AWS_BUCKET_NAME>", key="<KEY>", last_modified="<LAST_MODIFIED>"} <AGE_IN_SECONDS>
+aws_s3_object_key_size{bucket="<AWS_BUCKET_NAME>", key="<KEY>", last_modified="<LAST_MODIFIED>"} <OBJECT_SIZE_IN_BYTES>
 ```
 
-**Example command:**
+**Example command within kubernetes cluster:**
 
 ```
-docker run --rm -i \
-  -e DEBUG=1 \
-  -e PROM_PUSHGATEWAY_URL="http://prometheus-push-gateway-url" \
-  -e PROM_PUSHGATEWAY_PORT="9091" \
-  -e AWS_ACCESS_KEY_ID="XYZ" \
-  -e AWS_SECRET_ACCESS_KEY="XXYYZZ" \
-  -e AWS_BUCKET_NAME="backup.om3.cloud" \
-  -e AWS_DEFAULT_REGION="eu-central-1" \
-  aoepeople/prometheus-metrics:0.1 /bin/bash -c "/usr/local/bin/aws_s3_objects.sh"
+vuku k run check-s3-objects --rm -it \
+    --env PROM_PUSHGATEWAY_URL="http://prometheus-pushgateway.k28s-infrastructure" \
+    --env AWS_ACCESS_KEY_ID="OM3_META_ACCESS_KEY_ID" \
+    --env AWS_SECRET_ACCESS_KEY="OM3_META_SECRET_ACCESS_KEY" \
+    --env AWS_BUCKET_NAME="backup.om3.cloud" \
+    --env AWS_BUCKET_PATH="/backups/PROJECT/rds/STAGE" \
+    --env AWS_DEFAULT_REGION="eu-central-1" \
+    --image=aoepeople/prometheus-metrics:latest -- /bin/bash -c "/usr/local/bin/aws_s3_objects.sh"
+```
+
+**Example command within localhost:**
+
+```
+cd prometheus-metrics/
+docker run -d -p 9091:9091 --name prom_pushgateway prom/pushgateway
+export PROM_PUSHGATEWAY_HOST=$(docker exec $(docker ps -f name=prom_pushgateway --format "{{.ID}}") hostname -i)
+export AWS_PROFILE=om3-meta
+export AWS_DEFAULT_REGION=$(aws configure get region --profile ${AWS_PROFILE})
+docker run --rm -i -v $(PWD):/app -v $HOME/.aws:/root/.aws -w /app -e AWS_BUCKET_NAME="backup.om3.cloud" -e AWS_PROFILE="${AWS_PROFILE}" -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" -e PROM_PUSHGATEWAY_URL="http://${PROM_PUSHGATEWAY_HOST}" aoepeople/k8s_tools /bin/bash -c "/app/scripts/aws_s3_objects.sh"
+
 ```
 
 
--- prometheus-metrics/scripts/aws_s3_objects.sh
+++ prometheus-metrics/scripts/aws_s3_objects.sh
@@ -1,21 +1,13 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-#
-# Example
-# docker run -d -p 9091:9091 prom/pushgateway
-# docker run --rm -i -e DEBUG=1 -e PROM_PUSHGATEWAY_URL="http://127.0.0.1" -e AWS_ACCESS_KEY_ID="xxx" -e AWS_SECRET_ACCESS_KEY="xxx" -e AWS_BUCKET_NAME="backup.om3.cloud" -e AWS_DEFAULT_REGION="eu-central-1" aoepeople/prometheus-metrics:0.4 /bin/bash -c "/usr/local/bin/aws_s3_objects.sh"
-#
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
-function echoerr {
-    echo_red "============================================" 1>&2;
-    echo_red "ERROR: $@" 1>&2;
-    echo_red "============================================" 1>&2;
-}
-
+color_normal="\033[0m"; color_red="\033[0;31m"; color_green="\033[0;32m"; color_yellow="\033[0;34m";
+function echoerr { echo "============================================" 1>&2; echo_red "ERROR: $*" 1>&2;  echo "============================================" 1>&2; }
 function error_exit { echoerr "$1"; exit 1; }
-function echo_green { echo -e "\033[0;32m$1\033[0m"; }
-function echo_red { echo -e "\033[0;31m$1\033[0m"; }
+function echo_green { echo -e "${color_green}$1${color_normal}"; }
+function echo_red { echo -e "${color_red}$1${color_normal}"; }
+function echo_yellow { echo -e "${color_yellow}$1${color_normal}"; }
 
 if [ -z "${AWS_BUCKET_NAME}" ] ; then error_exit "AWS_BUCKET_NAME not set"; fi
 if [ -z "${AWS_DEFAULT_REGION}" ] ; then error_exit "AWS_DEFAULT_REGION not set"; fi
@@ -25,58 +17,38 @@ if [ -z "${PROM_PUSHGATEWAY_PORT}" ] ; then PROM_PUSHGATEWAY_PORT=9091; fi
 NOW=`date +%s`
 
 # https://github.com/prometheus/pushgateway#url
-COMPLETE_PROM_PUSHGATEWAY_URL="${PROM_PUSHGATEWAY_URL}:${PROM_PUSHGATEWAY_PORT}/metrics/job/s3_objects" # /metrics/job/<JOBNAME>{/<LABEL_NAME>/<LABEL_VALUE>}
-
-if [ "${DEBUG}" == "1" ]; then
-    echo 
-    echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
-    echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"
-    echo "AWS_BUCKET_NAME: ${AWS_BUCKET_NAME}"
-    echo "AWS_BUCKET_PATH: ${AWS_BUCKET_PATH}"
-    echo "AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}"
-    echo "PROM_PUSHGATEWAY_URL: ${PROM_PUSHGATEWAY_URL}"
-    echo "PROM_PUSHGATEWAY_PORT: ${PROM_PUSHGATEWAY_PORT}"
-    echo "COMPLETE_PROM_PUSHGATEWAY_URL: ${COMPLETE_PROM_PUSHGATEWAY_URL}"
-    echo "NOW: ${NOW}"
-    echo 
-fi 
+COMPLETE_PROM_PUSHGATEWAY_URL="${PROM_PUSHGATEWAY_URL}:${PROM_PUSHGATEWAY_PORT}/metrics/job/aws_s3_objects" # /metrics/job/<JOBNAME>{/<LABEL_NAME>/<LABEL_VALUE>}
 
 if [ "${PROCCESS_ONLY_LATEST_OBJECT_IN_PATH}" == "1" ] ; then 
-    echo "> aws --region \"${AWS_DEFAULT_REGION}\" s3 ls s3://\"${AWS_BUCKET_NAME}${AWS_BUCKET_PATH}\" --recursive | sort | tail -n 1 | awk '{print $4}'"
     s3list=`aws --region "${AWS_DEFAULT_REGION}" s3 ls s3://"${AWS_BUCKET_NAME}${AWS_BUCKET_PATH}" --recursive | sort | tail -n 1 | awk '{print $4}'` || error_exit "Failed list bucket"
 else 
-    echo "> aws --region \"${AWS_DEFAULT_REGION}\" s3 ls s3://\"${AWS_BUCKET_NAME}${AWS_BUCKET_PATH}\" --recursive | sort | awk '{print $4}'"
     s3list=`aws --region "${AWS_DEFAULT_REGION}" s3 ls s3://"${AWS_BUCKET_NAME}${AWS_BUCKET_PATH}" --recursive | sort | awk '{print $4}'` || error_exit "Failed list bucket"
 fi
 
-for key in $s3list
+TMPDIR=$(mktemp -d)
+trap "rm -rf ${TMPDIR}" EXIT
+
+for KEY in $s3list
 do
-   label=`echo ${key//\//_}`
-   label=`echo ${label//./_}`
+    echo
+    echo ">> AWS_BUCKET_NAME: ${AWS_BUCKET_NAME}"
+    echo ">> KEY: ${KEY}"
 
-    if [ "${DEBUG}" == "1" ]; then
-        echo 
-        echo ">> KEY: ${key}" 
-        echo ">> LABEL: ${label}" 
-    fi
-
-    OBJECT_META_DATA=$(aws --region "${AWS_DEFAULT_REGION}" s3api head-object --bucket "${AWS_BUCKET_NAME}" --key "${key}") || error_exit "Failed fetch object head"
+    OBJECT_META_DATA=$(aws --region "${AWS_DEFAULT_REGION}" s3api head-object --bucket "${AWS_BUCKET_NAME}" --key "${KEY}") || error_exit "Failed fetch object head"
     OBJECT_SIZE=$(echo $OBJECT_META_DATA | jq -r '.ContentLength') || error_exit "Failed to get ContentLenght via jq" # Size of the body in bytes
     LAST_MODIFIED_RAW=$(echo $OBJECT_META_DATA | jq -r '.LastModified') || error_exit "Failed to get LastModified via jq" # Last modified date of the object (timestamp)
     LAST_MODIFIED=$(date -d "${LAST_MODIFIED_RAW}" +"%s") || error_exit "Failed to parse LastModified to unix timestamp" # Convert "Thu, 18 Jan 2018 09:00:16 GMT" to UNIX timestamp (just work on linux!!)
     AGE=$(expr $NOW - $LAST_MODIFIED) # seconds
 
-    if [ "${DEBUG}" == "1" ]; then
-        echo ">> OBJECT_SIZE: ${OBJECT_SIZE}"
-        echo ">> LAST_MODIFIED: ${LAST_MODIFIED_RAW} (timestamp: ${LAST_MODIFIED})"
-        echo ">> AGE: ${AGE} (seconds)"
-    fi
+    echo ">> OBJECT_SIZE: $(printf %s\\n ${OBJECT_SIZE} | numfmt --to=si) (${OBJECT_SIZE} bytes)"
+    echo ">> LAST_MODIFIED: ${LAST_MODIFIED_RAW} (timestamp: ${LAST_MODIFIED})"
+    echo ">> AGE: ${AGE} (seconds)"
 
-
-    cat <<EOF | curl --data-binary @- ${COMPLETE_PROM_PUSHGATEWAY_URL}/key/${label}
-s3_key_age{bucket="${AWS_BUCKET_NAME}"} ${AGE}
-s3_key_size{bucket="${AWS_BUCKET_NAME}"} ${OBJECT_SIZE}
-EOF
-
-echo "Done"
+    echo -e "aws_s3_object_key_age{bucket=\"${AWS_BUCKET_NAME}\", key=\"${KEY}\", last_modified=\"${LAST_MODIFIED_RAW}\"} ${AGE}" >> $TMPDIR/metrics.txt
+    echo -e "aws_s3_object_key_size{bucket=\"${AWS_BUCKET_NAME}\", key=\"${KEY}\", last_modified=\"${LAST_MODIFIED_RAW}\"} ${OBJECT_SIZE}" >> $TMPDIR/metrics.txt
 done
+echo
+
+cat $TMPDIR/metrics.txt | curl -s --data-binary @- ${COMPLETE_PROM_PUSHGATEWAY_URL} || error_exit "Failed to push to ${COMPLETE_PROM_PUSHGATEWAY_URL}"
+echo_green "Pushed successfully to ${COMPLETE_PROM_PUSHGATEWAY_URL}"
+echo_green "Done"
