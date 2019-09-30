c51b45716294dd554fcfc3dd8e6ab4f1bae12c26
Julian Kleinhans <julian.kleinhans@aoe.com>
2019-09-30 15:59:09 +0200

    Add hotfix to replace namespace and clustername

--- mysql-backup-s3/scripts/backup_create.sh
+++ mysql-backup-s3/scripts/backup_create.sh
@@ -14,6 +14,10 @@ if [ -z "${S3_BUCKET}" ] ; then error_exit "S3_BUCKET not set"; fi
 if [ -z "${S3_KEY}" ] ; then error_exit "S3_KEY not set"; fi
 if [ -z "${DBENGINE}" ] ; then error_exit "DBENGINE not set (mysql or postgres)"; fi
 
+# Hotfix
+if [ -n "${NAMESPACE}" ] ; then S3_KEY=$(echo ${S3_KEY} | sed "s/###NAMESPACE###/${NAMESPACE}/g"); fi
+if [ -n "${CLUSTER_NAME}" ] ; then S3_KEY=$(echo ${S3_KEY} | sed "s/###CLUSTER_NAME###/${CLUSTER_NAME}/g"); fi
+
 echo "Creating ${DBENGINE} dump from ${DBUSER}:*****@${DBHOST}/${DBNAME}"
 if [ "${DBENGINE}" == "mysql" ] ; then
 
--- mysql-backup-s3/scripts/backup_get.sh
+++ mysql-backup-s3/scripts/backup_get.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
+# Hotfix
+if [ -n "${NAMESPACE}" ] ; then S3_KEY=$(echo ${S3_KEY} | sed "s/###NAMESPACE###/${NAMESPACE}/g"); fi
+if [ -n "${CLUSTER_NAME}" ] ; then S3_KEY=$(echo ${S3_KEY} | sed "s/###CLUSTER_NAME###/${CLUSTER_NAME}/g"); fi
+
 echo "Downloading file s3://${S3_BUCKET}${S3_KEY}"
 aws --region "${AWS_DEFAULT_REGION}" s3 cp "s3://${S3_BUCKET}${S3_KEY}" /tmp/dump.sql.gz
